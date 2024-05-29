41a2e2bb55c0a941ef5faf7fe7a1394e88e7f8de
jvictornascimento <victojoao789@gmail.com>
2024-05-28 23:38:53 -0300

    feat: created the layer product

---
+++ src/main/java/com/example/jwt/JwtTest/models/Product.java
@@ -0,0 +1,37 @@
+package com.example.jwt.JwtTest.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+@Entity
+@Table(name = "TB_PRODUSCTS")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+
+public class Product implements Serializable {
+    private static final long serialVersionUID=1L;
+
+    @Id
+    @EqualsAndHashCode.Include
+    @ToString.Include
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ToString.Include
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    @ToString.Include
+    private String description;
+
+    @ToString.Include
+    private Double price;
+
+    @ToString.Include
+    private Integer Quantity;
+}
