0fd2ec117427ba67638449706dd8569c5cd46630
jvictornascimento <victojoao789@gmail.com>
2024-06-11 23:15:58 -0300

    feat: roles to user

--- src/main/java/com/example/jwt/JwtTest/models/User.java
+++ src/main/java/com/example/jwt/JwtTest/models/User.java
@@ -2,8 +2,14 @@ package com.example.jwt.JwtTest.models;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.io.Serial;
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "TB_USERS")
@@ -13,7 +19,8 @@ import java.io.Serializable;
 @AllArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @ToString
-public class User implements Serializable {
+public class User implements Serializable, UserDetails{
+    @Serial
     private static final long serialVersionUID=1l;
 
     @Id
@@ -28,5 +35,36 @@ public class User implements Serializable {
     @ToString.Include
     private String password;
 
-    private String role;
+    private UserRoles role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.role ==UserRoles.ADMIN)return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"),new SimpleGrantedAuthority("ROLE_USER"));
+        else return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
--- src/main/java/com/example/jwt/JwtTest/repositories/UserRepository.java
+++ src/main/java/com/example/jwt/JwtTest/repositories/UserRepository.java
@@ -2,8 +2,12 @@ package com.example.jwt.JwtTest.repositories;
 
 import com.example.jwt.JwtTest.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    UserDetails findByEmail(String email);
+
 }
